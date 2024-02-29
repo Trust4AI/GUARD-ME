2dafb10e207573c6e82bc2064b9f1d02b5a1e7a0
migromarj <miguelromeroarjona@gmail.com>
2024-02-29 13:09:10 +0100

    Change docker port dynamically according to env variables

--- src/api/config/models.ts
+++ src/api/config/models.ts
@@ -4,14 +4,14 @@ const models: { [name: string]: OllamaModel } = {
     gemma: {
         name: 'gemma:2b',
         host:
-            process.env.GEMMA_HOST ?? process.env.NODE_ENV === 'development'
+            process.env.GEMMA_HOST || process.env.NODE_ENV === 'development'
                 ? 'http://localhost:11434'
                 : 'http://gemma:11434',
     },
     'dolphin-phi': {
         name: 'dolphin-phi',
         host:
-            process.env.DOLPHIN_HOST ?? process.env.NODE_ENV === 'development'
+            process.env.DOLPHIN_HOST || process.env.NODE_ENV === 'development'
                 ? 'http://localhost:11435'
                 : 'http://dolphin-phi:11434',
     },
