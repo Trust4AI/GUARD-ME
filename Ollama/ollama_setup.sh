8886638334c8b6d53f1b0c4584d7abaa7c5faa63
migromarj <miguelromeroarjona@gmail.com>
2024-02-29 16:11:26 +0100

    Add different options for dockerization of models with Ollama

--- Ollama/ollama_setup.sh
+++ Ollama/ollama_setup.sh
@@ -1,7 +1,12 @@
 MODEL_NAME=$1
+MODELFILE_PATH=$2
 
 ollama serve & sleep 5
 
-ollama pull $MODEL_NAME
+if [ -z $MODELFILE_PATH ]; then
+    ollama pull $MODEL_NAME
+else
+    ollama create $MODEL_NAME -f "./"$MODELFILE_PATH
+fi
 
 ollama run $MODEL_NAME
