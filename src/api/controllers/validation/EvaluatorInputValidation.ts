0d8ec892a7bb68e71e847cbce40565037813fce4
migromarj <migromarj@alum.us.es>
2024-06-07 12:07:45 +0200

    Update postman collection examples

--- src/api/controllers/validation/EvaluatorInputValidation.ts
+++ src/api/controllers/validation/EvaluatorInputValidation.ts
@@ -11,17 +11,21 @@ const evaluate = [
         .isString()
         .isLength({ min: 1, max: 30 })
         .trim()
-        .withMessage('biasType must be a string with length between 1 and 30'),
+        .withMessage('bias_type must be a string with length between 1 and 30'),
     check('prompt_1')
         .isString()
         .isLength({ min: 1, max: 2000 })
         .trim()
-        .withMessage('prompt1 must be a string with length between 1 and 2000'),
+        .withMessage(
+            'prompt_1 must be a string with length between 1 and 2000'
+        ),
     check('prompt_2')
         .isString()
         .isLength({ min: 1, max: 2000 })
         .trim()
-        .withMessage('prompt2 must be a string with length between 1 and 2000'),
+        .withMessage(
+            'prompt_2 must be a string with length between 1 and 2000'
+        ),
     check('attribute')
         .isString()
         .isLength({ min: 1, max: 30 })
@@ -51,7 +55,7 @@ const evaluate = [
         .isLength({ min: 1, max: 30 })
         .trim()
         .withMessage(
-            'candidateModel must be a string with length between 1 and 30'
+            'candidate_model must be a string with length between 1 and 30'
         ),
     check('evaluation_method')
         .optional()
@@ -59,32 +63,32 @@ const evaluate = [
         .isIn(['attributeComparison', 'properNamesComparison', 'consistency'])
         .trim()
         .withMessage(
-            'evaluationMethod is optional but if provided must be a string with one of the values: attributeComparison, properNamesComparison, consistency'
+            'evaluation_method is optional but if provided must be a string with one of the values: attributeComparison, properNamesComparison, consistency'
         ),
     check('response_max_length')
         .optional()
         .isInt({ min: 1, max: 2000 })
         .withMessage(
-            'responseMaxLength is optional but must be an integer between 1 and 2000 if provided'
+            'response_max_length is optional but must be an integer between 1 and 2000 if provided'
         ),
     check('list_format_response')
         .optional()
         .isBoolean()
         .withMessage(
-            'listFormatResponse is optional but must be a boolean if provided'
+            'list_format_response is optional but must be a boolean if provided'
         ),
     check('exclude_bias_references')
         .optional()
         .isBoolean()
         .withMessage(
-            'excludeBiasReferences is optional but must be a boolean if provided'
+            'exclude_bias_references is optional but must be a boolean if provided'
         ),
     check('evaluator_model')
         .optional()
         .isString()
         .isIn(['gpt-4-0125-preview', 'gpt-3.5-turbo-0125'])
         .withMessage(
-            'generatorModel is optional but must be a string with one of the following values if provided: gpt-4-0125-preview, gpt-3.5-turbo-0125'
+            'evaluator_model is optional but must be a string with one of the following values if provided: gpt-4-0125-preview, gpt-3.5-turbo-0125'
         ),
 ]
 
