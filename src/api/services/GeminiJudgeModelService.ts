4ebef5434515d357125175023bba9a1dc4c77fd0
migromarj <miguelromeroarjona@gmail.com>
2025-01-27 10:10:33 +0100

    Add proxy configuration

--- src/api/services/GeminiJudgeModelService.ts
+++ src/api/services/GeminiJudgeModelService.ts
@@ -1,14 +1,8 @@
-import {
-    ChatSession,
-    GenerativeModel,
-    GoogleGenerativeAI,
-} from '@google/generative-ai'
 import config from '../config/config'
-import { GeminiGenerationConfig } from '../types'
+import { ProxyAgent } from 'undici'
 
 const geminiAPIKey: string = config.geminiAPIKey
-
-const genAI: GoogleGenerativeAI = new GoogleGenerativeAI(geminiAPIKey)
+const proxyURL: string = config.proxyURL
 
 class GeminiJudgeModelService {
     async fetchModelJudgment(
@@ -21,34 +15,51 @@ class GeminiJudgeModelService {
             throw new Error('[GUARD-ME] GEMINI_API_KEY is not defined')
         }
 
-        const model: GenerativeModel = genAI.getGenerativeModel({
-            model: judgeModel,
-        })
+        const url: string = `https://generativelanguage.googleapis.com/v1beta/models/${judgeModel}:generateContent?key=${geminiAPIKey}`
 
-        const generationConfig: GeminiGenerationConfig = {
-            temperature: judgeTemperature,
-            topP: 0.95,
-            topK: 40,
-            maxOutputTokens: 8192,
-            response_mime_type: 'text/plain',
+        const headers: Record<string, string> = {
+            'Content-Type': 'application/json',
         }
 
-        const chatSession: ChatSession = model.startChat({
-            generationConfig,
-            history: [
+        const data: Record<string, any> = {
+            contents: [
                 {
                     role: 'user',
                     parts: [{ text: systemPrompt }],
                 },
+                {
+                    role: 'user',
+                    parts: [{ text: userPrompt }],
+                },
             ],
-        })
+            generationConfig: {
+                temperature: judgeTemperature,
+                topP: 0.95,
+                topK: 40,
+                maxOutputTokens: 8192,
+                response_mime_type: 'text/plain',
+            },
+        }
 
-        const result = await chatSession.sendMessage(userPrompt)
+        const fetchContent: RequestInit & {
+            dispatcher?: ProxyAgent
+        } = {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(data),
+        }
 
-        const content = result.response.text()
+        if (proxyURL) {
+            const dispatcher = new ProxyAgent(proxyURL)
+            fetchContent.dispatcher = dispatcher
+        }
 
-        if (content) {
-            return content
+        const content = await fetch(url, fetchContent).then((res) => res.json())
+
+        const response = content.candidates[0].content.parts[0].text
+
+        if (response) {
+            return response
         }
         throw new Error('[GUARD-ME] No content found in Gemini response')
     }
