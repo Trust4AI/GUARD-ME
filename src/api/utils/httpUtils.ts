260b93c2c254f69084b34f5ca546fbdd9913a903
migromarj <miguelromeroarjona@gmail.com>
2025-01-27 10:53:24 +0100

    Update fetch to GENIE

--- src/api/utils/httpUtils.ts
+++ src/api/utils/httpUtils.ts
@@ -1,17 +1,30 @@
+import { ProxyAgent } from 'undici'
+import config from '../config/config'
+
 const sendRequestToGenie = async (
     genieBaseUrl: string,
     requestBody: object
 ): Promise<string> => {
     let response: Response
+    const proxyURL: string = config.proxyURL
 
     try {
-        response = await fetch(`${genieBaseUrl}/models/execute`, {
+        const fetchContent: RequestInit & {
+            dispatcher?: ProxyAgent
+        } = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(requestBody),
-        })
+        }
+
+        if (proxyURL) {
+            const dispatcher = new ProxyAgent(proxyURL)
+            fetchContent.dispatcher = dispatcher
+        }
+
+        response = await fetch(`${genieBaseUrl}/models/execute`, fetchContent)
     } catch (error: any) {
         throw new Error(
             `[GUARD-ME] Fetch error when sending request to GENIE: ${error.message}`
